a49ec1fa04f2d196c70efb4da80967011a19c5ed
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-10-21 16:28:37 +0600

    style: adjust padding in TruckFleetsHero component for improved layout

--- src/components/truckfleetsComponents/truck-fleets-hero.tsx
+++ src/components/truckfleetsComponents/truck-fleets-hero.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const TruckFleetsHero = () => {
   return (
-    <section className="bg-background2 px-6 py-6 sm:py-8 md:py-16 lg:py-20 text-center">
+    <section className="bg-background2 px-6 py-6 sm:py-8 md:py-16  text-center">
       <div className="max-w-3xl mx-auto px-4">
         <h1 className="text-lg sm:text-xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-primary mb-1 sm:mb-2 md:mb-4">
           Our fleets of 10+ trucks

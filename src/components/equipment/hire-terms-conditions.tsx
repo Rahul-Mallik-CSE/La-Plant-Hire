e359929a7bfad80314350b4645573e35762f2d1a
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-10-22 17:33:05 +0600

    feat: improve layout and styling in HireTermsConditions component for better user experience

--- src/components/equipment/hire-terms-conditions.tsx
+++ src/components/equipment/hire-terms-conditions.tsx
@@ -5,13 +5,13 @@ import { Button } from "../ui/button";
 
 const HireTermsConditions = () => {
   return (
-    <div>
+    <div className="flex flex-col justify-center lg:block items-center">
       <h2 className="text-xl md:text-3xl lg:text-4xl font-bold text-primary mb-2 sm:mb-3 md:mb-4">
         Dry Hire Terms & Conditions
       </h2>
 
-      <div className="space-y-6 mb-8">
-        <div className="flex gap-1">
+      <div className="space-y-2 md:space-y-3 lg:space-y-6 mb-4 sm:mb-6 lg:mb-8 flex flex-col ">
+        <div className="flex gap-1   ">
           <div className="w-2.5 h-2.5 rounded-full bg-[#36b37e] flex items-center justify-center flex-shrink-0 mt-[4.5px]">
             <span className="text-white text-[7px]">✓</span>
           </div>
@@ -69,7 +69,7 @@ const HireTermsConditions = () => {
         </div>
       </div>
 
-      <Button className="bg-custom-orange border-1 md:border-2 border-primary text-primary px-4 py-2.5 rounded font-bold text-xs md:text-sm hover:bg-[#e67e00] transition">
+      <Button className="bg-custom-orange border-1 md:border-2 border-primary text-primary px-2 md:px-4 h-7 md:h-8 lg:h-9 rounded font-bold text-xs md:text-sm hover:bg-[#e67e00] transition">
         DOWNLOAD HIRE TERMS PDF
       </Button>
     </div>

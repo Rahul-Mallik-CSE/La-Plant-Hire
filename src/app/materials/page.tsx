7d3d4aae2be468cd9968496cbd68a8d53b0605a4
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-10-23 16:13:37 +0600

    feat: create MaterialPage component with CommonHero for displaying truck fleet information

---
+++ src/app/materials/page.tsx
@@ -0,0 +1,19 @@
+/** @format */
+
+import CommonHero from "@/components/Common/CommonHero";
+import React from "react";
+
+const MaterialPage = () => {
+  return (
+    <div>
+      {/* hero section */}
+      <CommonHero
+        title="Our fleets of 10+ trucks"
+        description="Professional grade trucks for every hauling need. From single body trucks to heavy-duty truck and dog combinations, we have the right vehicle for your project."
+        bgColor="bg-background2"
+      />
+    </div>
+  );
+};
+
+export default MaterialPage;

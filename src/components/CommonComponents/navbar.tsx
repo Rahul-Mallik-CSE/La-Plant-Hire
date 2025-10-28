8e7c769c147fc2adaca47dd5a2847cee6b390bf1
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-10-28 14:23:33 +0600

    feat: enhance Navbar with dynamic active link styling and client-side routing

--- src/components/CommonComponents/navbar.tsx
+++ src/components/CommonComponents/navbar.tsx
@@ -1,5 +1,7 @@
 /** @format */
 
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -10,8 +12,10 @@ import {
 import { Menu } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
   return (
     <header className="bg-white ">
       <div className="max-w-[2000px] mx-auto px-4 sm:px-6 lg:px-8 xl:px-16 py-3 flex items-center justify-between">
@@ -32,34 +36,44 @@ export default function Navbar() {
         </div>
 
         {/* Desktop Navigation */}
-        <nav className="hidden sm:flex items-center gap-1 bg-primary rounded-lg px-2 lg:px-6 py-1">
+        <nav className="hidden sm:flex items-center gap-1 bg-primary rounded-lg px-2  py-1">
           <Link
             href="/"
-            className="text-white text-sm font-medium px-1 lg:px-3 py-1 hover:bg-primary/80 rounded-full transition"
+            className={`text-white text-sm font-medium px-1 lg:px-3 py-1 rounded-xs transition ${
+              pathname === "/" ? "bg-gray-600" : "hover:bg-gray-700"
+            }`}
           >
             Home
           </Link>
           <Link
             href="/truck-fleets"
-            className="text-white text-sm font-medium px-1 lg:px-3 py-1 hover:bg-primary/80 rounded-full transition"
+            className={`text-white text-sm font-medium px-1 lg:px-3 py-1 rounded-xs transition ${
+              pathname === "/truck-fleets" ? "bg-gray-600" : "hover:bg-gray-700"
+            }`}
           >
             Truck Fleets
           </Link>
           <Link
             href="/equipment"
-            className="text-white text-sm font-medium px-1 lg:px-3 py-1 hover:bg-primary/80 rounded-full transition"
+            className={`text-white text-sm font-medium px-1 lg:px-3 py-1 rounded-xs transition ${
+              pathname === "/equipment" ? "bg-gray-600" : "hover:bg-gray-700"
+            }`}
           >
             Equipment
           </Link>
           <Link
             href="/materials"
-            className="text-white text-sm font-medium px-1 lg:px-3 py-1 hover:bg-primary/80 rounded-full transition"
+            className={`text-white text-sm font-medium px-1 lg:px-3 py-1 rounded-xs transition ${
+              pathname === "/materials" ? "bg-gray-600" : "hover:bg-gray-700"
+            }`}
           >
             Materials
           </Link>
           <Link
             href="/contact"
-            className="text-white text-sm font-medium px-1 lg:px-3 py-1 hover:bg-primary/80 rounded-full transition"
+            className={`text-white text-sm font-medium px-1 lg:px-3 py-1 rounded-xs transition ${
+              pathname === "/contact" ? "bg-gray-600" : "hover:bg-gray-700"
+            }`}
           >
             Contact
           </Link>
@@ -81,27 +95,62 @@ export default function Navbar() {
               align="end"
               className="w-12 block sm:hidden border-0 ml-6 mt-0"
             >
-              <DropdownMenuItem asChild className="focus:bg-primary py-1">
+              <DropdownMenuItem
+                asChild
+                className={`py-1 ${
+                  pathname === "/"
+                    ? "bg-primary focus:bg-black text-white"
+                    : "focus:bg-gray-200 focus:text-black"
+                }`}
+              >
                 <Link href="/" className="w-full ">
                   Home
                 </Link>
               </DropdownMenuItem>
-              <DropdownMenuItem asChild className="focus:bg-primary py-1">
+              <DropdownMenuItem
+                asChild
+                className={`py-1 ${
+                  pathname === "/truck-fleets"
+                    ? "bg-primary focus:bg-black text-white"
+                    : "focus:bg-gray-200 focus:text-black"
+                }`}
+              >
                 <Link href="/truck-fleets" className="w-full ">
                   Truck Fleets
                 </Link>
               </DropdownMenuItem>
-              <DropdownMenuItem asChild className="focus:bg-primary py-1">
+              <DropdownMenuItem
+                asChild
+                className={`py-1 ${
+                  pathname === "/equipment"
+                    ? "bg-primary focus:bg-black text-white"
+                    : "focus:bg-gray-200 focus:text-black"
+                }`}
+              >
                 <Link href="/equipment" className="w-full">
                   Equipment
                 </Link>
               </DropdownMenuItem>
-              <DropdownMenuItem asChild className="focus:bg-primary py-1">
+              <DropdownMenuItem
+                asChild
+                className={`py-1 ${
+                  pathname === "/materials"
+                    ? "bg-primary focus:bg-black text-white"
+                    : "focus:bg-gray-200 focus:text-black"
+                }`}
+              >
                 <Link href="/materials" className="w-full">
                   Materials
                 </Link>
               </DropdownMenuItem>
-              <DropdownMenuItem asChild className="focus:bg-primary py-1">
+              <DropdownMenuItem
+                asChild
+                className={`py-1 ${
+                  pathname === "/contact"
+                    ? "bg-primary focus:bg-black text-white"
+                    : "focus:bg-gray-200 focus:text-black"
+                }`}
+              >
                 <Link href="/contact" className="w-full">
                   Contact
                 </Link>
